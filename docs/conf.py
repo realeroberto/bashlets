0a50c87bca32e5d8252d4ea944fd5a342b48fc2d
Roberto Reale <roberto.reale82@gmail.com>
2016-11-08 16:14:57 +0100

    Remove githubpages docs format.

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
